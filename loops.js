cf8b1004bb00e305816a1185138113415c933aa2
Joshua Seabolt <jseabolt45@hotmail.com>
2018-11-13 15:14:02 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -1,3 +1,14 @@
 for(var i = 1; 1 < 100; i++) {
   console.log("Hello World the " + i + "time.");
 }
+
+function forLoop([i]) {
+  for(i=0; i<26;i++){
+    if(i===1) {
+    console.log("I am ${i} strange loop.");
+  }
+  else{
+    console.log("I am ${i} strange loops.");
+  }
+  
+}
